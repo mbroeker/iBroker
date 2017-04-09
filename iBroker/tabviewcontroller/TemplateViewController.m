a0a01d2ee9c1b32036dc139d559ae113106fc294
Markus Bröker <broeker.markus@googlemail.com>
2017-04-09 22:54:59 +0200

    Bug Fix: Der Button muss umbenannt werden...

--- iBroker/tabviewcontroller/TemplateViewController.m
+++ iBroker/tabviewcontroller/TemplateViewController.m
@@ -265,7 +265,8 @@
  * Übersicht mit richtigen Live-Werten
  */
 - (void)updateOverview {
-
+    self.dismissButton.title = @"Dashboard";
+    
     double total = [self calculate:@"EUR"];
 
     NSLog(@"Übersicht");
