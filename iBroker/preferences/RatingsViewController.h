1e9b15d12205a6f998490009c75eaeaa4e100eb1
Markus Bröker <broeker.markus@googlemail.com>
2017-04-26 04:04:38 +0200

    Preferences Scene aka Einstellungen hinzu gefügt

---
+++ iBroker/preferences/RatingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  RatingsViewController.h
+//  iBroker
+//
+//  Created by Markus Bröker on 26.04.17.
+//  Copyright © 2017 Markus Bröker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RatingsViewController : NSViewController
+- (IBAction)saveAction:(id)sender;
+@end
---
+++ iBroker/preferences/RatingsViewController.m
@@ -0,0 +1,20 @@
+//
+//  RatingsViewController.m
+//  iBroker
+//
+//  Created by Markus Bröker on 26.04.17.
+//  Copyright © 2017 Markus Bröker. All rights reserved.
+//
+
+#import "RatingsViewController.h"
+
+@implementation RatingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)saveAction:(id)sender {
+    NSLog(@"Noch nicht implementiert");
+}
+@end
