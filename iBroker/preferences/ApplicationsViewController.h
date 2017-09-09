8db09a7c086e5adb385192469592e7af171d3b38
Markus Bröker <broeker.markus@googlemail.com>
2017-09-09 20:43:17 +0200

    FileChooser for Applications

--- iBroker/preferences/ApplicationsViewController.h
+++ iBroker/preferences/ApplicationsViewController.h
@@ -23,16 +23,30 @@
 @property (strong) IBOutlet NSTextField *asset10Field;
 
 // Images List
-@property (strong) IBOutlet NSImageView *asset1ImageView;
-@property (strong) IBOutlet NSImageView *asset2ImageView;
-@property (strong) IBOutlet NSImageView *asset3ImageView;
-@property (strong) IBOutlet NSImageView *asset4ImageView;
-@property (strong) IBOutlet NSImageView *asset5ImageView;
-@property (strong) IBOutlet NSImageView *asset6ImageView;
-@property (strong) IBOutlet NSImageView *asset7ImageView;
-@property (strong) IBOutlet NSImageView *asset8ImageView;
-@property (strong) IBOutlet NSImageView *asset9ImageView;
-@property (strong) IBOutlet NSImageView *asset10ImageView;
+@property (strong) IBOutlet NSButton *asset1ImageButton;
+@property (strong) IBOutlet NSButton *asset2ImageButton;
+@property (strong) IBOutlet NSButton *asset3ImageButton;
+@property (strong) IBOutlet NSButton *asset4ImageButton;
+@property (strong) IBOutlet NSButton *asset5ImageButton;
+
+@property (strong) IBOutlet NSButton *asset6ImageButton;
+@property (strong) IBOutlet NSButton *asset7ImageButton;
+@property (strong) IBOutlet NSButton *asset8ImageButton;
+@property (strong) IBOutlet NSButton *asset9ImageButton;
+@property (strong) IBOutlet NSButton *asset10ImageButton;
+
+// Button Actions
+- (IBAction)asset1ClickedAction:(id)sender;
+- (IBAction)asset2ClickedAction:(id)sender;
+- (IBAction)asset3ClickedAction:(id)sender;
+- (IBAction)asset4ClickedAction:(id)sender;
+- (IBAction)asset5ClickedAction:(id)sender;
+
+- (IBAction)asset6ClickedAction:(id)sender;
+- (IBAction)asset7ClickedAction:(id)sender;
+- (IBAction)asset8ClickedAction:(id)sender;
+- (IBAction)asset9ClickedAction:(id)sender;
+- (IBAction)asset10ClickedAction:(id)sender;
 
 // Action Handler
 - (IBAction)saveAction:(id)sender;
--- iBroker/preferences/ApplicationsViewController.m
+++ iBroker/preferences/ApplicationsViewController.m
@@ -29,17 +29,17 @@
     self.asset10Field.placeholderString = ASSET10_DESC;
 
     // Images List
-    self.asset1ImageView.image = [NSImage imageNamed:ASSET1];
-    self.asset2ImageView.image = [NSImage imageNamed:ASSET2];
-    self.asset3ImageView.image = [NSImage imageNamed:ASSET3];
-    self.asset4ImageView.image = [NSImage imageNamed:ASSET4];
-    self.asset5ImageView.image = [NSImage imageNamed:ASSET5];
+    self.asset1ImageButton.image = [NSImage imageNamed:ASSET1];
+    self.asset2ImageButton.image = [NSImage imageNamed:ASSET2];
+    self.asset3ImageButton.image = [NSImage imageNamed:ASSET3];
+    self.asset4ImageButton.image = [NSImage imageNamed:ASSET4];
+    self.asset5ImageButton.image = [NSImage imageNamed:ASSET5];
 
-    self.asset6ImageView.image = [NSImage imageNamed:ASSET6];
-    self.asset7ImageView.image = [NSImage imageNamed:ASSET7];
-    self.asset8ImageView.image = [NSImage imageNamed:ASSET8];
-    self.asset9ImageView.image = [NSImage imageNamed:ASSET9];
-    self.asset10ImageView.image = [NSImage imageNamed:ASSET10];    
+    self.asset6ImageButton.image = [NSImage imageNamed:ASSET6];
+    self.asset7ImageButton.image = [NSImage imageNamed:ASSET7];
+    self.asset8ImageButton.image = [NSImage imageNamed:ASSET8];
+    self.asset9ImageButton.image = [NSImage imageNamed:ASSET9];
+    self.asset10ImageButton.image = [NSImage imageNamed:ASSET10];
 
     [self updateView];
 }
@@ -92,6 +92,141 @@
 
     // Gepeicherte Daten neu einlesen...
     [self updateView];
+}
+
+/**
+ * File Chooser für Applications
+ *
+ */
+- (NSURL *)pickApplication {
+    NSOpenPanel *panel = [NSOpenPanel openPanel];
+    [panel setCanChooseFiles:YES];
+    [panel setCanChooseDirectories:YES];
+    [panel setDirectoryURL:[NSURL URLWithString:@"/Applications/"]];
+
+    NSInteger clicked = [panel runModal];
+
+    if (clicked == NSFileHandlingPanelOKButton) {
+        return [panel URL];
+    }
+
+    return nil;
+}
+
+/**
+ * Action Handler für Asset1
+ */
+- (IBAction)asset1ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset1Field.stringValue = url.path;
+    }
+}
+
+/**
+ * Action Handler für Asset2
+ */
+- (IBAction)asset2ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset2Field.stringValue = url.path;
+    }
+}
+
+/**
+ * Action Handler für Asset3
+ */
+- (IBAction)asset3ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset3Field.stringValue = url.path;
+    }
+
+}
+
+/**
+ * Action Handler für Asset4
+ */
+- (IBAction)asset4ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset4Field.stringValue = url.path;
+    }
+
+}
+
+/**
+ * Action Handler für Asset5
+ */
+- (IBAction)asset5ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset5Field.stringValue = url.path;
+    }
+
+}
+
+/**
+ * Action Handler für Asset6
+ */
+- (IBAction)asset6ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset6Field.stringValue = url.path;
+    }
+}
+
+/**
+ * Action Handler für Asset7
+ */
+- (IBAction)asset7ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset7Field.stringValue = url.path;
+    }
+}
+
+/**
+ * Action Handler für Asset8
+ */
+- (IBAction)asset8ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset8Field.stringValue = url.path;
+    }
+
+}
+
+/**
+ * Action Handler für Asset9
+ */
+- (IBAction)asset9ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset9Field.stringValue = url.path;
+    }
+
+}
+
+/**
+ * Action Handler für Asset10
+ */
+- (IBAction)asset10ClickedAction:(id)sender {
+    NSURL *url = [self pickApplication];
+
+    if (url != nil) {
+        self.asset10Field.stringValue = url.path;
+    }
+
 }
 
 @end
